5023c01288af82555332225221831b139380ecd3
Kade Markoux <Skywolfgamestudios@gmail.com>
2023-11-25 15:22:54 -0500

    Added Editor Config + Camera and SpringArm

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -2,12 +2,20 @@
 
 
 #include "SCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
 
 }
 
@@ -29,6 +37,8 @@ void ASCharacter::Tick(float DeltaTime)
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("MoveForward", this, &ASCharacter::MoveForward);
 
 }
 
